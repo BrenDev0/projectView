b56c52b29631566ff94e954418f57c491980335c
Brendan Lahey <lahey1991@gmail.com>
2024-10-03 13:58:48 -0500

    WIP: home page component layout

--- private/include/pages/home_page.php
+++ private/include/pages/home_page.php
@@ -4,18 +4,13 @@
 include '../private/include/partials/html_head.php';
 ?>
 <body id="home-body"> 
-   <?php
-    include '../private/include/partials/mobile_header.php';
-    include '../private/include/partials/dt_header.php';
-   ?>
-   <main id="home-main">
+    <!-- Mobile -->
+   <main id="home-main-mobile">
    <?php 
-    // diplayed only on mobile
+    include '../private/include/partials/mobile_header.php';
     include '../private/include/partials/mobile_nav.php';
     include '../private/include/partials/toolbar.php';
-    //nav bar for desktop
-    include '../private/include/partials/nav_bar.php';
-    // New project modal
+    // Modals
     include '../private/include/partials/new_project_modal.php';
     include '../private/include/partials/new_idea_modal.php';
     // Recent projects component
@@ -26,8 +21,29 @@ include '../private/include/partials/html_head.php';
         <div class="dashboard-element"></div>
     </div>
    </main> 
+
+   <!-- Desktop -->
+   <main id="home-main">
+    <?php
+    include '../private/include/partials/dt_header.php';
+    include '../private/include/partials/nav_bar.php';
+    ?>
+    <div class="con h-con">
+        <div class="v-con ha-center stacked">
+        <?php
+        include '../private/include/partials/recent_projects_table.php';
+        ?>
+        <div id="hours">Hours this week</div>
+        </div>
+        <div class="long">Todays tasks</div>
+        <div class="v-con ha-center stacked">
+            <div>new project form</div>
+            <div>new idea form</div>
+        </div>
+    </div>
+    </main>
 </body>
 <?php
-require '../private/include/partials/script.php'
+require '../private/include/partials/script.php';
 ?>
 </html>
