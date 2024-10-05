f15ccbefc1139777683d4ec7209240bca9e35ad8
Brendan Lahey <lahey1991@gmail.com>
2024-10-05 12:12:00 -0500

    WIP: project class

--- private/classes/Project.php
+++ private/classes/Project.php
@@ -7,7 +7,7 @@ class Project{
         $this->conn = db_connect(); 
     }
 
-    function new_project($name, $type, $description){
+    function add_project($name, $type, $description){
         $sql = 'INSERT INTO projects (name, type, description) VALUES (:name, :type, :description)';
         $stmt = $this->conn->prepare($sql);
         $stmt->execute(array(
@@ -15,7 +15,14 @@ class Project{
             ':type'=> $type,
             ':decription' => $description
         ));
-
+        // Return the project id 
+        $query = 'SELECT project_id FROM projects WHERE name = :name AND description = :description';
+        $stmt = $this->conn->prepare($query);
+        $id = $stmt->execute(array(
+            ':name' => $name,
+            ':description' => $description
+        ));
+        return $id;
     }
 
     function get_project($project_id){
@@ -23,6 +30,12 @@ class Project{
         $stmt = $this->conn->query($sql);
         $project = $stmt->fetch(PDO::FETCH_ASSOC); 
         return $project;
+    }
+
+    function delete_project($project_id){
+        $sql = "DELETE * FROM projects WHERE project_id = $project_id";
+        $this->conn->query($sql);
+        return; 
     }
 }
 ?>
