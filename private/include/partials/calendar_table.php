421660927edb5136041966a533d1c49fa7d06d0f
Brendan Lahey <lahey1991@gmail.com>
2024-10-11 11:52:15 -0500

    WIP: calender functionallity and styling

--- private/include/partials/calendar_table.php
+++ private/include/partials/calendar_table.php
@@ -3,17 +3,9 @@ $year = date('Y');
 $month = date('m');
 ?>
 <div id="calendar-con">
-    <div class="date-select-con wide h-con ha-center va-center">
+    <div class="date-select-con wide h-con ha-center va-center" id="date-select-con">
         <select  name="month" id="month-select"></select>
         <select name="year" id="year-select"></select>
     </div>
-    <div id="days-con">
-        <div class="day-names" style="grid-column-start: 0; grid-column-end: 1; grid-row-start: 0; grid-row-end: 1;">Sunday</div>
-        <div class="day-names" style="grid-column-start: 1; grid-column-end: 2; grid-row-start: 0; grid-row-end: 1;">Monday</div>
-        <div class="day-names" style="grid-column-start: 2; grid-column-end: 3; grid-row-start: 0; grid-row-end: 1;">Tuesday</div>
-        <div class="day-names" style="grid-column-start: 3; grid-column-end: 4; grid-row-start: 0; grid-row-end: 1;">Wedensday</div>
-        <div class="day-names" style="grid-column-start: 4; grid-column-end: 5; grid-row-start: 0; grid-row-end: 1;">Thursday</div>
-        <div class="day-names" style="grid-column-start: 5; grid-column-end: 6; grid-row-start: 0; grid-row-end: 1;">Friday</div>
-        <div class="day-names" style="grid-column-start: 6; grid-column-end: 7;grid-row-start: 0; grid-row-end: 1;">Saturday</div>
-    </div>
+    <div id="days-con"></div>
 </div>
--- private/js/calendar.js
+++ private/js/calendar.js
@@ -3,8 +3,68 @@ export function createCalendar(month = new Date().getMonth() + 1, year = new Dat
     const totalDays = daysInMonth(year, month);
     const endingSpaces = new Date(`${month}/${totalDays}/${year}`).getDay();
     const calendar = document.getElementById('days-con');
-    let rowStart = 1;
+    const monthSelect = document.getElementById('month-select');
+    const yearSelect = document.getElementById('year-select')
 
+    //add function to selects
+    monthSelect.addEventListener('change', () => createCalendar(parseInt(monthSelect.value), parseInt(yearSelect.value)));
+    yearSelect.addEventListener('change', () => createCalendar(parseInt(monthSelect.value), parseInt(yearSelect.value)));
+
+    //clear calendar 
+    clearCalendar(calendar, monthSelect, yearSelect);
+    
+    //populate date month select options
+    for(let i = 1; i < 13; i++){
+        if(i === month){
+            let option = document.createElement('option');
+            option.setAttribute('value', i);
+            option.setAttribute('selected', 'selected');
+            let content = document.createTextNode(getMonthName(i));
+            option.append(content);
+            monthSelect.append(option);
+        }else {
+            let option = document.createElement('option');
+            option.setAttribute('value', i);
+            let content = document.createTextNode(getMonthName(i));
+            option.append(content);
+            monthSelect.append(option);
+        }
+    }
+
+    //populate year select options
+    for(let i = 0; i < 5; i++){
+        let option = document.createElement('option');
+        let content = year + i;
+        if (content === year){
+            option.setAttribute('value', content);
+            option.setAttribute('selected', 'selected');
+            option.append(content);
+            yearSelect.append(option);
+        }else{
+            option.setAttribute('value', content);
+            option.append(content);
+            yearSelect.append(option);
+        }
+    }
+
+    //populate week day names
+    for(let i = 0; i < 7; i++){
+        const days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+        let colEnd = i + 1;
+        const weekDay = document.createElement('div');
+        const content = document.createTextNode(days[i]);
+        weekDay.appendChild(content);
+        weekDay.setAttribute('class', 'day-names');
+        weekDay.style.gridColumnStart = i;
+        weekDay.style.gridColumnEnd = colEnd;
+        weekDay.style.gridRowStart = 0;
+        weekDay.style.gridRowEnd = 1;
+        calendar.append(weekDay);
+
+    }
+
+    let rowStart = 1;
+    //populate calendar beginning spaces
     for(let i = 0; i < startingSpaces; i++){
         let colEnd = i + 1;
         const space = document.createElement('div');
@@ -15,6 +75,7 @@ export function createCalendar(month = new Date().getMonth() + 1, year = new Dat
         space.style.gridRowEnd = 2;
         calendar.append(space);
     }
+    //populate calendar days
     for(let i = 1; i < totalDays + 1; i++){
         let rowEnd = rowStart + 1;
         let colStart =  new Date(`${month}/${i}/${year}`).getDay();
@@ -28,11 +89,12 @@ export function createCalendar(month = new Date().getMonth() + 1, year = new Dat
         const dayNumber = document.createTextNode(i);
         day.appendChild(dayNumber);
         calendar.append(day);
-
+        //change row
         if(colStart == 6){
             rowStart = rowStart + 1;
         }
     }
+    //populate ending spaces
     for(let i = endingSpaces + 1; i < 7; i++){
         let rowEnd = rowStart + 1;
         let colEnd = i + 1;
@@ -48,10 +110,35 @@ export function createCalendar(month = new Date().getMonth() + 1, year = new Dat
     return
 }
 
+// Get number of days in month
 function daysInMonth(year, month){
     return new Date(year, month, 0).getDate();
 }
 
+// Get Month name
+function getMonthName(monthNumber){
+    const date = new Date();
+    date.setMonth(monthNumber - 1);
+
+    return date.toLocaleDateString('en-US', {month: 'long'})
+}
+
+// clearing calendar before render
+function clearCalendar(calendar, selectMonth, selectYear){
+    while(calendar.lastChild){
+        calendar.removeChild(calendar.lastChild)
+    }
+
+    while(selectMonth.lastChild){
+        selectMonth.removeChild(selectMonth.lastChild)
+    }
+
+    while(selectYear.lastChild){
+        selectYear.removeChild(selectYear.lastChild)
+    }
+}
+
+// render calendar on load
 document.addEventListener('DOMContentLoaded', () => {
     createCalendar();
     console.log('Calendar Loaded.')
